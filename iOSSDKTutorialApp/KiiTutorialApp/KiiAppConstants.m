a1bdd51f648cfb6aad388d90fad40318042ceac3
Ryuji OCHI <ryuji.ochi@kii.com>
2013-11-18 14:31:09 +0900

    Change Application ID/Key of iOSSDKTutorialApp

--- iOSSDKTutorialApp/KiiTutorialApp/KiiAppConstants.m
+++ iOSSDKTutorialApp/KiiTutorialApp/KiiAppConstants.m
@@ -10,8 +10,8 @@
 
 @implementation KiiAppConstants
 
-NSString *const KII_APP_ID = @"98a3f7c5";
-NSString *const KII_APP_KEY = @"4a7d1a252dbb660a2cb5cc18ee015ee9";
+NSString *const KII_APP_ID = @"__APP_ID__";
+NSString *const KII_APP_KEY = @"__APP_KEY__";
 KiiSite const KII_APP_SITE = kiiSiteUS;
 NSString *const KII_APP_BUCKET_NAME = @"tutorial";
 
