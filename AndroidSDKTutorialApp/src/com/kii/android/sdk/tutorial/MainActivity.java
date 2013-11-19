42cfa7181d91a8425fdaaacda9dae8aaf2fa0b5f
Ryuji OCHI <ryuji.ochi@kii.com>
2013-11-19 19:34:00 +0900

    Adjust EditText hint size and add soft keyboard hidden function to MainActivity for AndroidSDKTutorialApp

--- AndroidSDKTutorialApp/src/com/kii/android/sdk/tutorial/MainActivity.java
+++ AndroidSDKTutorialApp/src/com/kii/android/sdk/tutorial/MainActivity.java
@@ -1,10 +1,14 @@
 package com.kii.android.sdk.tutorial;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
+import android.text.method.PasswordTransformationMethod;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -30,9 +34,21 @@ public class MainActivity extends Activity {
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
         Kii.initialize(AppConstants.APP_ID, AppConstants.APP_KEY,
                 AppConstants.APP_SITE);
+
+        // Specify EditText type now, due to control font size of EditText hint.
+        EditText editPassword = (EditText) this
+                .findViewById(R.id.password_field);
+        editPassword
+                .setTransformationMethod(new PasswordTransformationMethod());
+        editPassword.setInputType(InputType.TYPE_CLASS_TEXT
+                | InputType.TYPE_TEXT_VARIATION_PASSWORD);
     }
 
     public void onLoginButtonClicked(View v) {
+        // Hide soft keyboard
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);
+
         progressBar.setVisibility(View.VISIBLE);
         Log.v(TAG, "login clicked");
         String username = mUsernameField.getText().toString();
@@ -46,6 +62,10 @@ public class MainActivity extends Activity {
     }
 
     public void onSignupButtonClicked(View v) {
+        // Hide soft keyboard
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);
+
         progressBar.setVisibility(View.VISIBLE);
 
         String username = mUsernameField.getText().toString();
