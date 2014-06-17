29e3a85150a093a7b3c1e78c86ee72357e379b7c
Moshiur <moshiur.rahman@kii.com>
2014-06-17 16:30:44 +0900

    Fix fragment loading when attach file. (fix of https://github.com/KiiCorp/kanban/issues/313)

--- AndroidSDKTutorialApp/src/com/kii/android/sdk/tutorial/MainActivity.java
+++ AndroidSDKTutorialApp/src/com/kii/android/sdk/tutorial/MainActivity.java
@@ -2,7 +2,9 @@ package com.kii.android.sdk.tutorial;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
@@ -11,12 +13,21 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        loadFragment(new LoginFragment());
+        loadFragment();
     }
     
-    private void loadFragment(Fragment fragment){
+    private void loadFragment(){
+        FragmentManager fm = getFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.mainFragment);
+        if(fragment == null)
+            fragment = new LoginFragment();
         FragmentTransaction ft = getFragmentManager().beginTransaction();
         ft.replace(R.id.mainFragment, fragment);
         ft.commit();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+       super.onActivityResult(requestCode, resultCode, data);
     }
 }
